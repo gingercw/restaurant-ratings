2ced9ffef3492c7d9bdf721b02a013e62b7361c6
gingercw <gingercw@gmail.com>
2022-02-11 04:58:19 +0000

    finished working on restaurant ratings in dictionary

---
+++ ratings.py
@@ -0,0 +1,28 @@
+"""Restaurant rating lister."""
+# create function
+# open file
+# remove spaces
+# define the restaurant name and the ratings
+
+def show_restaurant_ratings(scores_file):
+  '''Stores restaurant and ratings in a dictionary.'''
+
+  restaurant_ratings = open(scores_file)
+
+  restaurant_ratings_dict = {}
+
+  for line in restaurant_ratings:
+    line = line.rstrip()
+    words = line.split(":")
+    restaurant = words[0]
+    rating = words[1]
+
+    restaurant_ratings_dict[restaurant] = rating
+
+  for restaurant, rating in sorted(restaurant_ratings_dict.items()):
+    print(f'{restaurant} is rated at {rating}.')    
+  # return sorted(restaurant_ratings_dict.items())
+
+
+show_restaurant_ratings("scores.txt")
+# put your code here
